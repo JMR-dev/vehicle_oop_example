0519061cdead576bf1f47e7142442a135ebdebdc
bytesparallel <jason.ross841@gmail.com>
2022-06-22 11:27:54 -0500

    removed unneeded defmain

--- fleet.py
+++ fleet.py
@@ -1,11 +1,10 @@
-def main():
-    class Fleet():
-        vehicles = {"7E19H2", "N9460G32581A3", "BN4681"}
-        def __init__(self, vtype, movspeed, travelmed, vehicleid):
-            self.vehicletype = vtype
-            self.movementspeed = movspeed
-            self.travelmedium = travelmed
-            self.uuid = vehicleid
+class Fleet():
+    vehicles = {"7E19H2", "N9460G32581A3", "BN4681"}
+    def __init__(self, vtype, movspeed, travelmed, vehicleid):
+        self.vehicletype = vtype
+        self.movementspeed = movspeed
+        self.travelmedium = travelmed
+        self.uuid = vehicleid
             
-        def displayAllFleetMembers(vehicles):
-            map(print(vehicles))
+    def displayAllFleetMembers(vehicles):
+        map(print(vehicles))
